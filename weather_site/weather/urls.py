92ed374fe4b9c5015033186502f9ce1e122c5386
aphelele-kubeka <aphelele.kubeka@gmail.com>
2021-07-05 10:51:05 -0700

    Moved notes to index page and added eror handling for cities not found

--- weather_site/weather/urls.py
+++ weather_site/weather/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index),
-    path('weather/weather-info.html', views.weather_info),
-    path('weather/editor.html/', views.editor, name='editor'),
+    path('', views.editor, name='editor'),
     path('delete_note/<int:noteid>/', views.delete_note, name='delete_note'),
+    path('weather/weather-info.html', views.weather_info),
 ]
--- weather_site/weather/views.py
+++ weather_site/weather/views.py
@@ -5,48 +5,6 @@ from .models import Note
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'weather/index.html', {
-    })
-
-def weather_info(request):
-    # read csv file with city names and latitudes and longitudes
-    df = pd.read_csv('worldcities.csv')
-    if 'city' in request.GET:
-        city = request.GET['city'] 
-        if df[df['city_ascii'] == city]['city_ascii'].any():
-
-            lat = df[df['city_ascii'] == city]['lat'] 
-            lon = df[df['city_ascii'] == city]['lng']
-
-            url = "https://climacell-microweather-v1.p.rapidapi.com/weather/realtime"
-
-            # query ClimaCell Weather API using latidude and longitude
-            querystring = {"unit_system": "si",
-            "fields": ["precipitation", "precipitation_type", "temp", "cloud_cover", "wind_speed",
-            "weather_code"], "lat": lat, "lon": lon}
-
-            # host and key found in ClimaCell Weather API Documentation
-            headers = {
-            'x-rapidapi-host': "climacell-microweather-v1.p.rapidapi.com",
-            'x-rapidapi-key': "64606952fcmsh4e754c5afa969d3p101001jsn250893a3dd43"
-            }
-
-            response = requests.request("GET", url, headers=headers, params=querystring).json()
-
-            # add response to a dictionary
-            context = {'city_name': city,
-            'temp': response['temp']['value'],
-            'weather_code': response['weather_code']['value'],
-            'wind_speed': response['wind_speed']['value'],
-            'precipitation_type': response['precipitation_type']['value'] }
-            return render(request, 'weather/weather-info.html', context)
-        else:
-            context = None
-    else:
-            context = None
-            return render(request, 'weather/weather-info.html', context)
-
 def editor(request):
     noteid = int(request.GET.get('noteid', 0))
     notes = Note.objects.all()
@@ -79,10 +37,48 @@ def editor(request):
         'note': note
     }
 
-    return render(request, 'weather/editor.html', context)
+    return render(request, 'weather/index.html', context)
 
 def delete_note(request, noteid):
     note = Note.objects.get(pk=noteid)
     note.delete()
 
     return redirect('/?noteid=0')
+
+def weather_info(request):
+    # read csv file with city names and latitudes and longitudes
+    df = pd.read_csv('worldcities.csv')
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if df[df['city_ascii'] == city]['city_ascii'].any():
+
+            lat = df[df['city_ascii'] == city]['lat'] 
+            lon = df[df['city_ascii'] == city]['lng']
+
+            url = "https://climacell-microweather-v1.p.rapidapi.com/weather/realtime"
+
+            # query ClimaCell Weather API using latidude and longitude
+            querystring = {"unit_system": "si",
+            "fields": ["precipitation", "precipitation_type", "temp", "cloud_cover", "wind_speed",
+            "weather_code"], "lat": lat, "lon": lon}
+
+            # host and key found in ClimaCell Weather API Documentation
+            headers = {
+            'x-rapidapi-host': "climacell-microweather-v1.p.rapidapi.com",
+            'x-rapidapi-key': "64606952fcmsh4e754c5afa969d3p101001jsn250893a3dd43"
+            }
+
+            response = requests.request("GET", url, headers=headers, params=querystring).json()
+
+            # add response to a dictionary
+            context = {'city_name': city,
+            'temp': response['temp']['value'],
+            'weather_code': response['weather_code']['value'],
+            'wind_speed': response['wind_speed']['value'],
+            'precipitation_type': response['precipitation_type']['value'] }
+            return render(request, 'weather/weather-info.html', context)
+        else:
+            context = None
+            return render(request, 'weather/city-not-found.html', context)
+    else:
+            context = None
